71df4f29318ea3b4cadb9c40de0352fa9dc9ad98
Karyn Herrera <karynherrera@gmail.com>
2018-08-04 13:10:50 -0400

    se agrega form para tarjeta

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 
 const btnAñadirLista = document.getElementById("btnAñadirL");
+const divLista = document.getElementById("tarjeta");
  btnAñadirLista.addEventListener('click',()=>{
-    console.log("holi");
+    divLista.innerHTML='<div class="card"><div class="form-group"><input type="text" class="form-control" placeholder="Añadir una Lista"><button type="button" class="btn btn-primary">Añadir</button></form></div>';
+    btnAñadirLista.style.display="none";
  });
