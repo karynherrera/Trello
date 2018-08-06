852b5e8c85e8ba32d02cf69ee2becf6265230526
Karyn Herrera <karynherrera@gmail.com>
2018-08-06 10:50:01 -0400

    se añade tarjeta y funcionalidad de botones

---
+++ src/js/data.js
@@ -0,0 +1,9 @@
+const addToList = (nameList) => {
+  let arrayTareas = [];
+    for(let i=0; i<arrayTareas.length;i++){
+      arrayTareas[i] = nameList;
+      console.log("creado"+ arrayTareas[i]);
+    }
+  
+  
+ };
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,17 @@
 const btnAñadirLista = document.getElementById("btnAñadirL");
 const divLista = document.getElementById("tarjeta");
  btnAñadirLista.addEventListener('click',()=>{
-    divLista.innerHTML='<div class="card"><div class="form-group"><input type="text" class="form-control" placeholder="Añadir una Lista"><button type="button" class="btn btn-primary">Añadir</button></form></div>';
+    divLista.innerHTML='<div class="card"><div class="form-group"><input id="inputList" type="text" class="form-control" placeholder="Añadir una Lista"><button type="button" class="btn btn-primary" onclick="getInfoList()" >Añadir</button></form></div>';
     btnAñadirLista.style.display="none";
  });
+
+ const getInfoList = () => {
+  let listName=document.getElementById(inputList.value);
+  if(inputList.value===""){
+    console.log("Input Vacio");
+    }else{
+    console.log("Input lleno");
+    addToList(inputList.value);
+  }
+  
+ };
