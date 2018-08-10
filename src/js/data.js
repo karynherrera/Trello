f04866a72816f3f44e9350105d737d5e96ee2a22
Karyn Herrera <karynherrera@gmail.com>
2018-08-10 11:53:21 -0400

    se coordina funcionalidad de datos d elas tarjetas

--- src/js/data.js
+++ src/js/data.js
@@ -1,13 +1,41 @@
-let arrayTareas = [];
-let arrayCards = [];
-const addToList = (nameList) => {
-    arrayTareas.push(nameList);
-    console.log(arrayTareas);
-  return(card(nameList));
- };
+let arrayLista = [];
 
- const addToCards = (nameCard) => {
+
+const addToList = (idlist, nameList) => {
+  let lista = new Object();
+  lista.id = idlist;
+  lista.name = nameList;
+  lista.tareas = [];
+  arrayLista.push(lista);
+  console.log(arrayLista);
+  let i = arrayLista.findIndex(e=>e.id===idlist);
+  let arrListas = arrayLista[i];
+  console.log(arrListas);
+  return (card(arrListas));
+  /*
+  arrayTareas.push(nameList);
+  console.log(arrayTareas);
+  return (card(nameList));*/
+};
+
+const addToCards = (idList,nameCard) => {
+  let arrayCards = [];
+  let result;
+  let found = arrayLista.find(item => {
+    if (item.id === idList) {
+      item.tareas.push(nameCard);
+      return result = true;
+    } else {
+      return result = false;
+    } 
+  });
+  if(result){
+    //console.log("item.id "+item.id+" idList "+idList);
+  //nameList.tareas.push(nameCard);
   arrayCards.push(nameCard);
-    console.log(arrayCards);
-  return(newCards(nameCard));
- };
+  console.log(arrayLista);
+  return (newCards(nameCard));
+  }
+  
+  
+};
--- src/js/pruebas.js
+++ src/js/pruebas.js
@@ -20,7 +20,7 @@ const showList=()=>{
  
   divLista.innerHTML += `<section id="${IdCard}" class="card show">
   <div class="form-group"><input id="${inputListId}" type="text" class="form-control" placeholder="Añadir una Lista" autofocus="autofocus">
-  <button id="${btnListaId}" type="button" class="btnForm btn-primary" onclick="getInfoList()" >Añadir Lista
+  <button id="${btnListaId}" type="button" class="btnForm btn-primary" onclick="getInfoList(IdCard)" >Añadir Lista
   </button>
   <button id="${btnCloseId}" type="button" class="btnCerrar btn-primary" onclick="close()" ><i class="fas fa-times" onclick="close()">
   </i>
@@ -43,7 +43,7 @@ const activate=()=>{
   btnListaId.disabled=false;
 }
 
-const getInfoList = () => {
+const getInfoList = (idLista) => {
   const cardVacia = document.getElementById(IdCard);
   cardVacia.style.display="none";
   const inputList = document.getElementById(inputListId);
@@ -52,26 +52,33 @@ const getInfoList = () => {
     inputList.focus();
   } else {
     btnListaId.disabled=false;
-    addToList(inputList.value);
+    addToList(idLista,inputList.value);
   }
 }
 
-const card = (nameList) => {
+const card = (arrayList) => {
+
+  console.log(arrayList.name);
+  nameLista=arrayList.name;
+  idLista=arrayList.id;
   countCard=countCard+1;
   tarjetaNewId = "tarjetaNew"+countCard;
   addCardId = "addCard"+countCard;
   cardsId = "cards"+countCard;
-  divLista.innerHTML += `<div class="card"><div class="form-group" id="${tarjetaNewId}"><strong><p>${nameList}</p></strong><div id="${cardsId}"></div><div id="${addCardId}"><button class="btnForm btn-outline-success m-0 p-0 my-2 my-sm-0 space btnCard"  type="button"  onclick="createCard()">
+  plusCard = "agregarCard"+countCard;
+  divLista.innerHTML += `<div class="card"><div class="form-group" id="${tarjetaNewId}"><strong><p>${nameLista}</p></strong><div id="${cardsId}"></div><div id="${addCardId}"><button id="${plusCard}" class="btnForm btn-outline-success m-0 p-0 my-2 my-sm-0 space btnCard"  type="button"  onclick="createCard(idLista)">
   <h6 class="m-0 p-0"><i class="fas fa-plus space"></i>Añadir Tarjeta...</h6>
 </button></div></div></div>`;
 }
 
-const createCard = () => {
+const createCard = (idLista) => {
+  console.log("holi entré a create card");
   inputCardId = "inputCard"+countCard;
   btnAddCardId = "btnAddCard"+countCard;
+  btnCerrar = "btnCloseCard"+countCard;
   const divCard = document.getElementById(addCardId);
   divCard.innerHTML = `<textarea class="inputCard" id="${inputCardId}" placeholder="Titulo de la Tarjeta..." onclick="activar()" autofocus="autofocus"></textarea>
-  <button id="${btnAddCardId}" type="button" class=" btn-primary btnForm" onclick="getInfoCards()">Añadir Tarjeta</button><button type="button" onclick="close()" class="btnCerrar btn-primary"><i class="fas fa-times"></i></button>
+  <button id="${btnAddCardId}" type="button" class=" btn-primary btnForm" onclick="getInfoCards(idLista)">Añadir Tarjeta</button><button id="${btnCerrar}" type="button" onclick="close()" class="btnCerrar btn-primary"><i class="fas fa-times"></i></button>
   `;
   
 }
@@ -80,15 +87,14 @@ const activar=()=>{
   btnAddCardId.disabled=false; 
 }
 
-const getInfoCards = () => {
-  
+const getInfoCards = (idList) => {
   let inputCard = document.getElementById(inputCardId);
   if (inputCard.value === "") {
     btnAddCardId.disabled=true;
     inputCard.focus();
   } else {
     btnAddCardId.disabled=false;
-    addToCards(inputCard.value);
+    addToCards(idList,inputCard.value);
   }
 };
 
